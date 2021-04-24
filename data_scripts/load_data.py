6646e26bf62a23c19db33c30e5ebf878d89d1cee
Szymon Borowy <sb406106@students.mimuw.edu.pl>
2021-04-24 15:22:15 +0200

    added possesed stocks and portfolio value and some sample data

--- data_scripts/load_data.py
+++ data_scripts/load_data.py
@@ -1,15 +1,30 @@
 import csv
 import os
-from models import Stock, StockData
+from models import Stock, StockData, User, BoughtStocks
 from config import db
 
 path = '/home/szymon/Pulpit/stockdata/'
+
+
+db.create_all()
+user1 = User('user1', 'user1', 5151.5)
+user2 = User('user2', 'user2', 217711.4)
+user3 = User('user3', 'user3', 912.3)
+user1.save_to_db()
+user2.save_to_db()
+user3.save_to_db()
 
 
 def load_file(name):
     file_reader = csv.reader(open(path + name), delimiter=',')
     stock = Stock(name.split('.')[0], True)
     stock.save_to_db()
+    bought_stocks1 = BoughtStocks(user1.id, stock.id, 1.0)
+    bought_stocks2 = BoughtStocks(user2.id, stock.id, 1.0)
+    bought_stocks3 = BoughtStocks(user3.id, stock.id, 1.0)
+    bought_stocks1.save_to_db()
+    bought_stocks2.save_to_db()
+    bought_stocks3.save_to_db()
     for idx, row in enumerate(file_reader):
         if idx == 0:
             continue
@@ -17,6 +32,5 @@ def load_file(name):
         stock_data.save_to_db()
 
 
-db.create_all()
 for filename in os.listdir(path):
     load_file(filename)
--- models/user.py
+++ models/user.py
@@ -1,16 +1,17 @@
 from config import db
 
-
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String)
     password = db.Column(db.String)
+    portfolio_value = db.Column(db.Float)
 
-    def __init__(self, login, password):
+    def __init__(self, login, password, portfolio_value=0.0):
         self.login = login
         self.password = password
+        self.portfolio_value = portfolio_value
 
     def json(self):
         return {
@@ -22,6 +23,10 @@ class User(db.Model):
     @classmethod
     def search_for_user(cls, log, passw):
         return cls.query.filter_by(login=log, password=passw).first()
+
+    @classmethod
+    def get_user_value(cls, id):
+        return cls.query.filter_by(id=id).first().portfolio_value
 
     def save_to_db(self):
         db.session.add(self)
